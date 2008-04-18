8c9e2dc4997400900ecdf8e77efa6fd086d299fe
Marcel Gruenauer <hanekomu@gmail.com>
2008-04-18 14:08:56 +0000

    r6023@nbgr:  marcel | 2008-04-18 16:08:29 +0200  lang/perl/App-sync_cpantesters: initial commit
    git-svn-id: http://svn.coderepos.org/share/lang/perl/App-sync_cpantesters/tags/App-sync_cpantesters-0.04@9728 d0d07461-0603-4401-acd4-de1884942a52

---
+++ lib/App/sync_cpantesters.pm
@@ -0,0 +1,34 @@
+package App::sync_cpantesters;
+
+use warnings;
+use strict;
+
+
+our $VERSION = '0.04';
+
+
+1;
+
+
+__END__
+
+{% USE p = PodGenerated %}
+
+=head1 NAME
+
+App::sync_cpantesters - Sync CPAN testers failure reports to local dir
+
+=head1 SYNOPSIS
+
+None.
+
+=head1 DESCRIPTION
+
+This is just a marker package so you can install this distribution from the
+CPAN shell.
+
+{% PROCESS standard_pod %}
+
+=cut
+
+
